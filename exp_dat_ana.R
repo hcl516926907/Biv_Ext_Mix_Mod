4c59df734c0d76b77cc441f2847fc3f2080e4eb7
chenglei_hu <chenglei_hu@163.com>
2022-03-02 23:17:34 +0000

    Add a function to calculate tail dependence coefficients. Draw the chi plot and the chi bar plot

--- exp_dat_ana.R
+++ exp_dat_ana.R
@@ -71,5 +71,42 @@ plot(riv1.unif, riv2.unif, main = "Weekly River Flow, mariginally uniform",
 
 
 
+u.seq <- seq(0,1,0.01)
+u <- 0.99
 
+tail.coef <- function(x,y,u){
+  n <- length(x)
+  
+  x.0 <- which(x<=u)
+  x.1 <- which(x>u)
+  y.0 <- which(y<=u)
+  y.1 <- which(y>u)
+  
+  x.p.0 <- length(x.0)/n
+  x.p.1 <- length(x.1)/n
+  y.p.0 <- length(y.0)/n
+  y.p.1 <- length(y.1)/n
+  
+  p.0.0 <- length(intersect(x.0,y.0))/length(y.0)
+  p.0.1 <- length(intersect(x.0,y.1))/length(y.1)
+  p.1.0 <- length(intersect(x.1,y.0))/length(y.0)
+  p.1.1 <- length(intersect(x.1,y.1))/length(y.1)
+  
+  res <- c(x.p.0, x.p.1, y.p.0, y.p.1,
+           p.0.0, p.0.1, p.1.0, p.1.1)
+  names(res) <- c('x.p.0', 'x.p.1', 'y.p.0', 'y.p.1',
+                  'p.0.0', 'p.0.1', 'p.1.0', 'p.1.1')
+  return(res)
+}
+
+tail.coef(riv1.unif,riv2.unif,u)
+
+
+coef.tab <- sapply(u.seq, tail.coef, x=riv1.unif, y=riv2.unif)
+
+chi <- coef.tab['p.1.1',]
+plot(u.seq, chi, type='l', main="Chi Plot", xlab='u', ylab='chi')
+
+chi.bar <- 2*log(coef.tab['x.p.1',])/(log(coef.tab['p.1.1',]*coef.tab['y.p.1',]))-1
+plot(u.seq, chi.bar, type='l', main="Chi Bar Plot", xlab='u', ylab='chi bar')
 
