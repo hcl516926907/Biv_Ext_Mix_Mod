9d3c2bbdecba56107b1d45883b2799e84e3eb3f1
chenglei_hu <chenglei_hu@163.com>
2022-03-02 12:12:13 +0000

    load river and pollutants data

--- exp_dat_ana.R
+++ exp_dat_ana.R
@@ -1,4 +1,22 @@
 
-dir.dat <- '/home/pgrad2/2448355h/My_PhD_Project/00_Dataset/'
+# data directory
+dir.dat <- '/home/pgrad2/2448355h/My_PhD_Project/00_Dataset'
 
-riv.dat.nam <- 'Puerto Rico Fit Dataset.dat'
+# name of Ruerto Rico rive dataset 
+riv.dat.fit.nam <- 'Puerto Rico Fit Dataset.dat'
+riv.dat.tes.nam <- 'Puerto Rico Test Data.dat'
+
+# name of Leeds air pollution dataset
+pol.dat.fit.nam <- 'Leeds Fit Dataset.dat'
+pol.dat.tes.nam <- 'Leeds Test Dataset.dat'
+
+
+# load all datasets
+riv.dat.fit <- read.table(file.path(dir.dat,riv.dat.fit.nam), header=TRUE)
+riv.dat.tes <- read.table(file.path(dir.dat,riv.dat.tes.nam), header=TRUE)
+riv.dat <- rbind(riv.dat.fit,riv.dat.tes)
+
+pol.dat.fit <- read.table(file.path(dir.dat,pol.dat.fit.nam), header=TRUE)
+pol.dat.tes <- read.table(file.path(dir.dat,pol.dat.tes.nam), header=TRUE)
+pol.dat <- rbind(pol.dat.fit,pol.dat.tes)
+
