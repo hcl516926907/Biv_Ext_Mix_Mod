4890e6937804f48103895db736e5bb544933a150
chenglei_hu <chenglei_hu@163.com>
2022-06-02 11:37:13 +0000

    Examine the marginal tail estimations under different thresholds

--- thres_influ.R
+++ thres_influ.R
@@ -10,11 +10,23 @@ load(file=file.path(dir.in,'simulation_data.RData'))
 
 #--------------------------------check threshold for each margin---------------------------------
 
+u.x <- u.x.p10
+u.z <- u.z.p10
+
+X <- X.p10
+Z <- Z.p10
+
+
+q1.x <- sum((X[,1]> u.x[1]))/length(X[,1]) 
+q2.x <- sum((X[,2]> u.x[2]))/length(X[,2]) 
+
+q1.z <- sum((Z[,1]> u.z[1]))/length(Z[,1]) 
+q2.z <- sum((Z[,2]> u.z[2]))/length(Z[,2]) 
 library(ismev)
-q <- 0.98
-dat <- Z.10to1[,2]
+q <- 0.95
+dat <- Z[,2]
 u <- quantile(dat,q)
 plot(dat)
-gpd.fitrange(dat, umin=6,umax=8,nint=20)
+gpd.fitrange(dat, umin=8,umax=12,nint=20)
 m1<-gpd.fit(dat,thresh=u)
 gpd.diag(m1)
