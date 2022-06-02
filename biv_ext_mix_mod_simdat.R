78dd90d761694a52ecacc18a6af9a26d78398f5c
chenglei_hu <chenglei_hu@163.com>
2022-06-02 11:26:45 +0000

    Amend the code of generating the bulk data from drawing samples from a normal distribution truncated below and above to drawing samples from a normal distribution truncated below, then dropping the tail part.
    Amend the tail proportion.

--- biv_ext_mix_mod_simdat.R
+++ biv_ext_mix_mod_simdat.R
@@ -13,7 +13,7 @@ dir.out <- '/home/pgrad2/2448355h/My_PhD_Project/01_Output/Biv_Ext_Mix_Mod/biv_e
 
 
 ############################################################################################
-#------------------------sample size ratio bulk:tail = 4:1----------------------------------
+#------------------------tail prop: 20%-------------------------------
 ############################################################################################
 
 set.seed(1234)
@@ -52,13 +52,17 @@ sigma <- matrix(c(1,rho,rho,0.8),ncol=2)
 
 # GP scale tail data combined with the bulk data
 set.seed(1234)
-X.bulk <- rtmvnorm(2000, mean=u.x-c(1,1), sigma=1*sigma, lower=c(0,0), upper=u.x)
-plot(X.bulk)
-X <- rbind(X.bulk, sweep(X.tail$X,2,u.x,"+"))
+X.bulk <- rtmvnorm(2500, mean=u.x-c(1,1), sigma=1*sigma, lower=c(0,0))
+cond.x <- t(t(X.bulk) < u.x)
+X.bulk2 <- X.bulk[rowSums(cond.x)==2,]
+plot(X.bulk2)
+print(dim(X.bulk2)[1])
+X <- rbind(X.bulk2, sweep(X.tail$X,2,u.x,"+"))
+print(dim(X)[1])
 plot(X,xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data')
 plot(X,pch=20, xlim=c(16,23),ylim=c(0,10),xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data')
 plot(X,pch=20, xlim=c(16,23),ylim=c(0,10),
-     xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data',col=c(rep('grey50',2000),rep('black',500)))
+     xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data',col=c(rep('grey50',dim(X.bulk2)[1]),rep('black',500)))
 segments(u.x[1], 0, u.x[1], u.x[2], col='red', lty=2)
 segments(0, u.x[2], u.x[1], u.x[2], col='red', lty=2)
 legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c(NA,NA,2),col = c('black', 'grey50', 'red'),         
@@ -67,25 +71,31 @@ chiplot(X, qlim=c(0.02,0.99), ask=F)
 
 # Exponential scale tail data combined with the bulk data
 set.seed(1234)
-Z.bulk <- rtmvnorm(2000, mean=u.z-c(2,2), sigma=6*sigma, lower=c(0,0), upper=u.z)
-Z <- rbind(Z.bulk, sweep(X.tail$Z,2,u.z,"+"))
+Z.bulk <- rtmvnorm(2700, mean=u.z-c(2,2), sigma=6*sigma, lower=c(0,0))
+cond.z <- t(t(Z.bulk) < u.z)
+Z.bulk2 <- Z.bulk[rowSums(cond.z)==2,]
+plot(Z.bulk2)
+print(dim(Z.bulk2)[1])
+Z <- rbind(Z.bulk2, sweep(X.tail$Z,2,u.z,"+"))
+print(dim(Z)[1])
 plot(Z,pch=20,xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data')
-plot(Z,pch=20, xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data',col=c(rep('grey50',2000),rep('black',500)))
+plot(Z,pch=20, xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data',col=c(rep('grey50',dim(Z.bulk2)[1]),rep('black',500)))
 segments(u.z[1], 0, u.z[1], u.z[2], col='red', lty=2)
 segments(0, u.z[2], u.z[1], u.z[2], col='red', lty=2)
 legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c(NA,NA,2),col = c('black', 'grey50', 'red'),         
        pch=c(20,20,NA)) 
 chiplot(Z, qlim=c(0.02,0.99), ask=F)
 
-
-X.4to1 <- X
-Z.4to1 <- Z
-u.x.4to1 <- u.x
-u.z.4to1 <- u.z
+print(500/dim(X)[1])
+print(500/dim(Z)[1])
+X.p20 <- X
+Z.p20 <- Z
+u.x.p20 <- u.x
+u.z.p20 <- u.z
 
 
 #############################################################################################
-#------------------------sample size ratio bulk:tail = 10:1----------------------------------
+#------------------------tail prop: 10%----------------------------------
 #############################################################################################
 set.seed(1234)
 
@@ -98,7 +108,7 @@ gamma<-c(-0.253,-0.035)
 # Final beta parameter fixed at zero in estimation, so needs to be relative to this
 
 # Simulate with (conditionally) GP(sig,gamma) and (conditionally) exponential margins 
-X.tail<-sim.RevExpU.MGPD(n=200,d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
+X.tail<-sim.RevExpU.MGPD(n=250,d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
 
 #  GP(sig,gamma)
 plot(X.tail$X, pch=20, xlab=expression(X[1]),ylab=expression(X[2]),xlim=c(-10,2))
@@ -116,13 +126,17 @@ sigma <- matrix(c(1,rho,rho,0.8),ncol=2)
 
 # GP scale tail data combined with the bulk data
 set.seed(1234)
-X.bulk <- rtmvnorm(2000, mean=u.x-c(1,1), sigma=1*sigma, lower=c(0,0), upper=u.x)
-plot(X.bulk)
-X <- rbind(X.bulk, sweep(X.tail$X,2,u.x,"+"))
+X.bulk <- rtmvnorm(2900, mean=u.x-c(1,1), sigma=1*sigma, lower=c(0,0))
+cond.x <- t(t(X.bulk) < u.x)
+X.bulk2 <- X.bulk[rowSums(cond.x)==2,]
+plot(X.bulk2)
+print(dim(X.bulk2)[1])
+X <- rbind(X.bulk2, sweep(X.tail$X,2,u.x,"+"))
+print(dim(X)[1])
 plot(X,xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data')
 plot(X,pch=20, xlim=c(16,23),ylim=c(0,10),xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data')
 plot(X,pch=20, xlim=c(16,23),ylim=c(0,10),
-     xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data',col=c(rep('grey50',2000),rep('black',200)))
+     xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data',col=c(rep('grey50',dim(X.bulk2)[1]),rep('black',250)))
 segments(u.x[1], 0, u.x[1], u.x[2], col='red', lty=2)
 segments(0, u.x[2], u.x[1], u.x[2], col='red', lty=2)
 legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c(NA,NA,2),col = c('black', 'grey50', 'red'),         
@@ -131,10 +145,15 @@ chiplot(X, qlim=c(0.02,0.99), ask=F)
 
 # Exponential scale tail data combined with the bulk data
 set.seed(1234)
-Z.bulk <- rtmvnorm(2000, mean=u.z-c(3,3), sigma=4*sigma, lower=c(0,0), upper=u.z)
-Z <- rbind(Z.bulk, sweep(X.tail$Z,2,u.z,"+"))
+Z.bulk <- rtmvnorm(2500, mean=u.z-c(3,3), sigma=4*sigma, lower=c(0,0))
+cond.z <- t(t(Z.bulk) < u.z)
+Z.bulk2 <- Z.bulk[rowSums(cond.z)==2,]
+plot(Z.bulk2)
+print(dim(Z.bulk2)[1])
+Z <- rbind(Z.bulk2, sweep(X.tail$Z,2,u.z,"+"))
+print(dim(Z)[1])
 plot(Z,pch=20,xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data')
-plot(Z,pch=20, xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data',col=c(rep('grey50',2000),rep('black',200)))
+plot(Z,pch=20, xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data',col=c(rep('grey50',dim(Z.bulk2)[1]),rep('black',250)))
 segments(u.z[1], 0, u.z[1], u.z[2], col='red', lty=2)
 segments(0, u.z[2], u.z[1], u.z[2], col='red', lty=2)
 legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c(NA,NA,2),col = c('black', 'grey50', 'red'),         
@@ -142,10 +161,12 @@ legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c
 chiplot(Z, qlim=c(0.02,0.99), ask=F)
 
 
-X.10to1 <- X
-Z.10to1 <- Z
-u.x.10to1 <- u.x
-u.z.10to1 <- u.z
+print(250/dim(X)[1])
+print(250/dim(Z)[1])
+X.p10 <- X
+Z.p10 <- Z
+u.x.p10 <- u.x
+u.z.p10 <- u.z
 
 #############################################################################################
 #------------------------sample size ratio bulk:tail = 20:1----------------------------------
@@ -161,7 +182,7 @@ gamma<-c(-0.253,-0.035)
 # Final beta parameter fixed at zero in estimation, so needs to be relative to this
 
 # Simulate with (conditionally) GP(sig,gamma) and (conditionally) exponential margins 
-X.tail<-sim.RevExpU.MGPD(n=100,d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
+X.tail<-sim.RevExpU.MGPD(n=125,d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
 
 #  GP(sig,gamma)
 plot(X.tail$X, pch=20, xlab=expression(X[1]),ylab=expression(X[2]),xlim=c(-10,2))
@@ -179,13 +200,17 @@ sigma <- matrix(c(1,rho,rho,0.8),ncol=2)
 
 # GP scale tail data combined with the bulk data
 set.seed(1234)
-X.bulk <- rtmvnorm(2000, mean=u.x-c(1.3,1.1), sigma=0.6*sigma, lower=c(0,0), upper=u.x)
-plot(X.bulk)
-X <- rbind(X.bulk, sweep(X.tail$X,2,u.x,"+"))
+X.bulk <- rtmvnorm(2600, mean=u.x-c(1.3,1.1), sigma=0.6*sigma, lower=c(0,0))
+cond.x <- t(t(X.bulk) < u.x)
+X.bulk2 <- X.bulk[rowSums(cond.x)==2,]
+plot(X.bulk2)
+print(dim(X.bulk2)[1])
+X <- rbind(X.bulk2, sweep(X.tail$X,2,u.x,"+"))
+print(dim(X)[1])
 plot(X,xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data')
 plot(X,pch=20, xlim=c(0,7),ylim=c(0,5),xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data')
 plot(X,pch=20, xlim=c(0,7),ylim=c(0,5),
-     xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data',col=c(rep('grey50',2000),rep('black',100)))
+     xlab='X1', ylab='X2', main='Simulated tNormal + mGP Data',col=c(rep('grey50',dim(X.bulk2)[1]),rep('black',125)))
 segments(u.x[1], 0, u.x[1], u.x[2], col='red', lty=2)
 segments(0, u.x[2], u.x[1], u.x[2], col='red', lty=2)
 legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c(NA,NA,2),col = c('black', 'grey50', 'red'),         
@@ -194,22 +219,28 @@ chiplot(X, qlim=c(0.02,0.99), ask=F)
 
 # Exponential scale tail data combined with the bulk data
 set.seed(1234)
-Z.bulk <- rtmvnorm(2000, mean=u.z-c(2,2), sigma=1.5*matrix(c(1,0.65,0.65,0.8),ncol=2), lower=c(0,0), upper=u.z)
-Z <- rbind(Z.bulk, sweep(X.tail$Z,2,u.z,"+"))
+Z.bulk <- rtmvnorm(2570, mean=u.z-c(1.7,1.7), sigma=1.2*matrix(c(1,0.65,0.65,0.8),ncol=2), lower=c(0,0))
+cond.z <- t(t(Z.bulk) < u.z)
+Z.bulk2 <- Z.bulk[rowSums(cond.z)==2,]
+plot(Z.bulk2)
+print(dim(Z.bulk2)[1])
+Z <- rbind(Z.bulk2, sweep(X.tail$Z,2,u.z,"+"))
+print(dim(Z)[1])
 plot(Z,pch=20,xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data')
-plot(Z,pch=20, xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data',col=c(rep('grey50',2000),rep('black',500)))
+plot(Z,pch=20, xlab='Z1', ylab='Z2', main='Simulated tNormal + std GP Data',col=c(rep('grey50',dim(Z.bulk2)[1]),rep('black',125)))
 segments(u.z[1], 0, u.z[1], u.z[2], col='red', lty=2)
 segments(0, u.z[2], u.z[1], u.z[2], col='red', lty=2)
 legend('bottomright',   legend = c("Tail Data", "Bulk Data", "Threshold"), lty=c(NA,NA,2),col = c('black', 'grey50', 'red'),         
        pch=c(20,20,NA)) 
 chiplot(Z, qlim=c(0.02,0.99), ask=F)
 
-X.20to1 <- X
-Z.20to1 <- Z
-u.x.20to1 <- u.x
-u.z.20to1 <- u.z
 
-save(X.4to1,Z.4to1,u.x.4to1,u.z.4to1, 
-     X.10to1,Z.10to1,u.x.10to1,u.z.10to1,
-     X.20to1,Z.20to1,u.x.20to1,u.z.20to1,
+X.p05 <- X
+Z.p05 <- Z
+u.x.p05 <- u.x
+u.z.p05 <- u.z
+
+save(X.p05,Z.p05,u.x.p05,u.z.p05, 
+     X.p10,Z.p10,u.x.p10,u.z.p10, 
+     X.p20,Z.p20,u.x.p20,u.z.p20, 
      file=file.path(dir.out,'simulation_data.RData'))
