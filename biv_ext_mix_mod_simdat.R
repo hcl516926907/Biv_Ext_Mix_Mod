3fc32f4ba513db0c8798f4f71c6e24dd6a67fe5f
chenglei_hu <chenglei_hu@163.com>
2023-03-27 22:44:43 +0000

    1. Compare the samples from the likelihood by MCMC and that from the simulation function 2. Calculate the coverage rate of the confidence interval.

--- biv_ext_mix_mod_simdat.R
+++ biv_ext_mix_mod_simdat.R
@@ -574,3 +574,341 @@ plot(Y)
 
 save(X1,X2, Y, u.x,
      file=file.path(dir.out,'simulation_data_non_stationary_extr_dep.RData'))
+
+
+#---------generate data with stationarity in the extremeral dependence by MCMC---------
+set.seed(1234)
+N <- 2500
+x1 <- rnorm(N,0,1)
+X1 <- cbind(rep(1,N), x1)
+X1 <-  sweep(X1, 2, c(0,mean(X1[,2])), '-')
+
+x2 <- rnorm(N,0,1)
+X2 <- cbind(rep(1,N), x2)
+X2 <-  sweep(X2, 2, c(0,mean(X2[,2])), '-')
+
+# beta.a1 <- c(0.5, 0.2)
+# beta.a2 <- c(0.5, -0.4)
+# beta.b1 <- c(0.25,-0.25)
+
+beta.a1 <- c(0.5, 0)
+beta.a2 <- c(-0.4, 0)
+beta.b1 <- c(0.25,0)
+
+d<-2
+a<- cbind(exp(X1%*%beta.a1), exp(X1%*%beta.a2))
+b <- cbind(X1%*%beta.b1, 0)
+sig<-c(0.571,0.451)
+gamma<-c(0.253,0.135)
+
+u.x <- c(7,7.2)
+
+mu <- c(5,5.41)
+rho=0.5
+sigma <- 1.5* matrix(c(1,rho,rho,0.8),ncol=2)
+p <- pmvnorm(lower=rep(0,2), upper=u.x, mean=mu, sigma=sigma, keepAttr = F)
+
+n.tail <- N-floor(N*p)
+Y.tail <- matrix(NA, nrow=n.tail,ncol=2)
+
+
+ll.powunif.GPD<-function(theta,x,u,a.ind,lam.ind,sig.ind,gamma.ind, lamfix=FALSE, balthresh=FALSE, marg.scale.ind,marg.shape.ind)
+{ 
+  x.mat.ind <- 1
+  if ((x[1]<=0)&(x[2]<=0)) return(-10e10)
+  
+  if (is.null(dim(x))){
+    d <- length(x)
+    x.mat.ind <- 0
+  }else{
+    d<-dim(x)[2]
+  }
+  
+  a<-theta[a.ind]
+  if(length(a)==1)
+  {
+    a<-rep(a,d)
+  }
+  
+  if(lamfix){
+    lam<-rep(1,d)
+  }else{
+    lam<-c(theta[lam.ind],1)
+  }
+  
+  if(balthresh){
+    lam<-1/(1+a)
+  }
+  
+  sig<-theta[sig.ind]
+  gamma<-theta[gamma.ind]
+  
+  sig<-sig[marg.scale.ind]
+  gamma<-gamma[marg.shape.ind]
+  
+  rej<-NULL
+  for(j in 1:d)
+  {
+    rej[j]<-gamma[j]<0 && any(x[,j]>-sig[j]/gamma[j])
+  }
+  
+  if(any(lam<0.01)||any(a<=0.01)||any(sig<=0.001)||any(rej)){return(-10e10)}
+  
+  ll.uc <- 0
+  ll.pc <- 0
+  if (!x.mat.ind){
+    uc <- comp.gt(x, u)
+    if (uc){
+      L <- fX.powunif(x=x, a=a, lam=lam, sig=sig, gamma=gamma)
+      ll.uc <- log(L)
+    }else{
+      L2 <- fX.powunif.cens(x=x, u=u, lam=lam, a=a, sig=sig, gamma=gamma)
+      ll.pc <- log(L2)
+    }
+  }else{
+    uc<-apply(x,1,comp.gt,u=u)
+    
+    x.uc<-x[uc,]
+    x.pc<-x[!uc,]
+    
+    L<-apply(x.uc,1,fX.powunif,a=a,lam=lam,sig=sig,gamma=gamma)
+    ll.uc<-sum(log(L))
+    
+    if(sum(!uc)>0)
+    {
+      L2<-apply(x.pc,1,fX.powunif.cens,a=a,lam=lam,u=u,sig=sig,gamma=gamma)
+      ll.pc<- sum(log(L2))
+    }
+  }
+  if (is.nan(ll.uc)|is.nan(ll.pc)|(ll.uc==-Inf)|(ll.uc==Inf)){
+    return(-10e10)
+  }
+  ll<- ll.uc+ll.pc
+  
+  
+  return(ll)
+}
+
+theta <- c(a[1,], exp(b[1,1]), sig, gamma)
+a.ind <- c(1, 2)
+b.ind <- 3
+sig.ind <- c(4, 5)
+gamma.ind <- c(6, 7)
+marg.scale.ind <- c(1, 2)
+marg.shape.ind <- c(1, 2)
+
+y <- c(1,0)
+ll.powunif.GPD(theta=theta,x=y,u=min(y)-0.01,a.ind=a.ind,
+               lam.ind=lam.ind,sig.ind=sig.ind,gamma.ind=gamma.ind,
+               marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind )
+
+
+nim_ll.powunif.GPD <- nimbleRcall(function(theta=double(1), x=double(1), u=double(0), a.ind=double(1),
+                                            lam.ind=double(0), sig.ind=double(1), gamma.ind=double(1), 
+                                            lamfix=logical(0, default = 0), balthresh=logical(0, default = 0), 
+                                            marg.scale.ind=double(1), marg.shape.ind=double(1)){}, 
+                                   Rfun = 'll.powunif.GPD',
+                                   returnType = double(0))
+
+
+dpowunif.GPD <- nimbleFunction(
+  run = function(x=double(1), y=double(1),
+                 a.ind=double(1), lam.ind=double(0), lamfix=logical(0, default = 0), 
+                 sig.ind=double(1), gamma.ind=double(1),
+                 log = logical(0, default = 0)) {
+    returnType(double(0))
+    
+    ll <- nim_ll.powunif.GPD(x=y, theta=x, u=min(y)-0.01, a.ind=a.ind,
+                                                lam.ind=lam.ind, sig.ind=sig.ind, gamma.ind=gamma.ind, 
+                                                lamfix=lamfix, balthresh=FALSE, 
+                                                marg.scale.ind=1:2, marg.shape.ind=1:2)
+    if (log) {
+      totalProb <- ll
+    }else{
+      totalProb <- exp(ll)
+    }
+    return(totalProb)
+  })
+
+dpowunif.GPD(x=theta, y=y, a.ind=a.ind, lam.ind=lam.ind, lamfix=FALSE,
+             sig.ind=sig.ind, gamma.ind=gamma.ind,log=1)
+
+rpowunif.GPD <- nimbleFunction(
+  run = function(n=integer(0), y=double(1), a.ind=double(1), lam.ind=double(0), lamfix=logical(0, default = 0), 
+                 sig.ind=double(1), gamma.ind=double(1)) {
+    returnType(double(1))
+    
+    totalProb <- 1:7
+    return(totalProb)
+  })
+
+registerDistributions(list(
+  dpowunif.GPD = list(
+    BUGSdist = "dpowunif.GPD(y, a.ind, lam.ind, lamfix, sig.ind, gamma.ind)",
+    types = c('value = double(1)', 'y = double(1)', 'a.ind = double(1)', 
+              'lam.ind = double(0)', 'lamfix = logical(0)', 'sig.ind = double(1)',
+              'gamma.ind = double(1)')
+  )))
+
+
+
+Simu.GPD.code <- nimbleCode({
+  
+  theta[1:7] ~ dpowunif.GPD(y=y[1:2], a.ind=1:2,
+                        lam.ind=3, lamfix=0, 
+                        sig.ind=4:5, gamma.ind=6:7)
+  y[1] ~ dunif(-100, 100)
+  y[2] ~ dunif(-100, 100)
+})
+
+
+
+Simu.GPD.model <- nimbleModel(Simu.GPD.code, constants = list(theta=theta),
+                                                            check = FALSE)
+
+Simu.GPD.model$setData(list(theta = theta))
+
+cSimu.GPD.model <- compileNimble(Simu.GPD.model)
+Simu.GPD.model.confg <- configureMCMC(Simu.GPD.model)
+
+Simu.GPD.model.MCMC<- buildMCMC(Simu.GPD.model.confg)
+
+cSimu.GPD.model.MCMC <- compileNimble(Simu.GPD.model.MCMC, project = Simu.GPD.model)
+
+t1 <- Sys.time()
+results <- runMCMC(cSimu.GPD.model.MCMC, niter = 60000,nburnin=20000,thin=4,
+                   summary = TRUE, WAIC = FALSE,setSeed = 1234)
+t2 <- Sys.time()
+print(t2-t1)
+
+plot(results$samples[,'y[2]'],type='l')
+plot(results$samples[,c('y[1]','y[2]')])
+# plot(density(results$samples[,c('y[1]')]))
+
+cond1 <- results$samples[,'y[1]']>0
+plot(density(results$samples[, ][cond1, 'y[1]']))
+
+cond2 <- results$samples[,'y[2]']>0
+plot(density(results$samples[, ][cond2, 'y[2]']))
+
+x <- seq(0,10,0.1)
+plot(x, dgpd(x, loc=0, scale=sig[1], shape=gamma[1]),type='l')
+sp.mg1 <- rgpd(1000, loc=0, scale=sig[1], shape=gamma[1])
+sp.mg2 <- rgpd(1000, loc=0, scale=sig[2], shape=gamma[2])
+qqplot(sp.mg1,results$samples[, ][cond1, 'y[1]'])
+abline(a=0, b=1)
+qqplot(sp.mg2,results$samples[, ][cond2, 'y[2]'])
+abline(a=0, b=1)
+
+plot(x, dgpd(x, loc=0, scale=sig[2], shape=gamma[2]),type='l')
+
+Y.tail.1 <- sim.RevExpU.MGPD(n=200,d=d, a=a[1,], beta=b[1,], sig=sig, gamma=gamma, MGPD = T,std=T)$X
+plot(Y.tail.1)
+# plot(density(Y.tail.1[,1]))
+
+cond1 <- Y.tail.1[,1]>0
+plot(density(Y.tail.1[cond1, 1]))
+qqplot(sp.mg1,Y.tail.1[cond1, 1])
+abline(a=0, b=1)
+
+cond2 <- Y.tail.1[,2]>0
+plot(density(Y.tail.1[cond2, 2]))
+qqplot(sp.mg2,Y.tail.1[cond2, 2])
+abline(a=0, b=1)
+
+x <- Y.tail.1
+u <- min(x)-0.01
+par <- theta
+opt1<-optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+           sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+par <- opt1$par
+opt1.1 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+par <- opt1.1$par
+opt1.2 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+par <- opt1.2$par
+opt1.3 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE,hessian=TRUE)
+
+opt1.3$par
+1.96*sqrt(diag(solve(opt1.3$hessian)))
+
+0.970 0.960 0.697 0.773 0.838 0.776 0.765
+
+N.sim <- 100
+par.mat <- matrix(NA, nrow=N.sim, ncol=length(theta))
+CI.width <- matrix(NA, nrow=N.sim, ncol=length(theta))
+for (i in 1:N.sim){
+  set.seed(i)
+  Y.tail.1 <- sim.RevExpU.MGPD(n=200,d=d, a=a[1,], beta=b[1,], sig=sig, gamma=gamma, MGPD = T,std=T)$X
+  x <- Y.tail.1
+  u <- min(x)-0.01
+  par <- theta
+  opt1<-optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+              sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+  par <- opt1$par
+  opt1.1 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                  sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+  par <- opt1.1$par
+  opt1.2 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                  sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+  par <- opt1.2$par
+  opt1.3 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                  sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE,hessian=TRUE)
+  
+  par.mat[i,] <- opt1.3$par
+  CI.width[i,] <- 1.96*sqrt(diag(solve(opt1.3$hessian)))
+  print(i)
+}
+
+upper <- par.mat + CI.width
+lower <- par.mat - CI.width
+within.ind <- rep(NA, length(theta))
+for (i in 1:length(theta)){
+  cond <- theta[i]>=lower[,i]
+  cond.nona <- cond[!is.na(cond)]
+  within.ind[i] <- sum(cond.nona) /length(cond.nona)
+}
+
+
+x <- results$samples[,c('y[1]','y[2]')]
+u <- min(x)-0.01
+par <- theta
+opt2<-optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+            sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+par <- opt2$par
+opt2.1 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+par <- opt2.1$par
+opt2.2 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE)
+
+par <- opt2.2$par
+opt2.3 <- optim(nll.powunif.GPD, par=par, u=u, x=x, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind, control=list(maxit=10000,reltol=1e-6),balthresh=FALSE,hessian=TRUE)
+
+-nll.powunif.GPD(theta=theta, x=x,u=u, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind )
+
+-nll.powunif.GPD(theta=opt2.1$par, x=x,u=u, lamfix=F, a.ind=a.ind, lam.ind=lam.ind,
+                 sig.ind=sig.ind, gamma.ind=gamma.ind, marg.scale.ind=marg.scale.ind, marg.shape.ind=marg.shape.ind )
+
+
+
+set.seed(1234)
+for (i in 1:n.tail){
+  Y.tail[i,] <- sim.RevExpU.MGPD(n=1,d=d, a=a[i,], beta=b[i,], sig=sig, gamma=gamma, MGPD = T,std=T)$X
+}
+
+# GP scale tail data combined with the bulk data
+set.seed(1234)
+Y.bulk <- rtmvnorm(floor(N*p), mean=mu, sigma=sigma, lower=c(0,0),upper=u.x)
+Y <- rbind(sweep(Y.tail,2,u.x,"+"), Y.bulk)
+plot(Y)
+# plot(Y.tail)
+# plot(Y.tail.raw)
+# lines(U, col='red')
+# 
+# save(X1,X2, Y, u.x,
+#      file=file.path(dir.out,'simulation_data_non_stationary_extr_dep.RData'))
