66ba6379f4be3e02157b85cb3453f16508297380
chenglei_hu <chenglei_hu@163.com>
2023-08-21 10:23:48 +0000

    Add the code for calculating the posterior dependence parameter

--- Air_Pollution_India/Air_Pollution_Analysis.R
+++ Air_Pollution_India/Air_Pollution_Analysis.R
@@ -54,9 +54,28 @@ pm2.5_no2 <- pollute.max %>%
            filter_at(vars(PM2.5,NO2), all_vars(!is.infinite(.))) %>% 
            select(PM2.5,NO2)
 print(dim(pm2.5_no2))
+plot(pm2.5_no2)
+
+pollute.mean <- pollute %>% group_by(To.Date)%>% 
+  summarise(PM2.5=mean(PM2.5, na.rm=T),
+            Ozone=mean(Ozone, na.rm=T),
+            NO=mean(NO, na.rm=T),
+            NO2=mean(NO2, na.rm=T),
+            NOx=mean(NOx, na.rm=T),
+            NH3=mean(NH3, na.rm=T),
+            PM10=mean(PM10, na.rm=T))
+
+pm2.5_no2_mean <- pollute.mean %>% 
+  filter_at(vars(PM2.5,NO2), all_vars(!is.na(.))) %>% 
+  select(PM2.5,NO2)
+print(dim(pm2.5_no2_mean))
+plot(pm2.5_no2_mean)
 
 
-load(file=file.path(dir.out, filename='Air_pollution_mvn_0.8_0.99.RData'))
+# load(file=file.path(dir.out, filename='Air_pollution_mvn_0.8_0.99.RData'))
+# load(file=file.path(dir.out, filename='Air_pollution_mvtn_0.5_0.99.RData'))
+# load(file=file.path(dir.out, filename='Air_pollution_mvtn_0.6_0.99_AFSlice.RData'))
+load(file=file.path(dir.out, filename='Air_pollution_mvtn_0.6_0.99_AFSlice_daily_mean.RData'))
 
 chain_out <- chain_res
 para.name <- colnames(chain_out[[1]]$samples)
@@ -80,10 +99,10 @@ samples.all <- rbind(chain_out[[1]]$samples[,],
 
 
 
-plot(samples.all[,'sds[1]'],type='l')
+plot(samples.all[,'sds[2]'],type='l')
 plot(samples.all[,'thres[1]'],type='l')
 plot(samples.all[,'thres[2]'],type='l')
-
+plot(samples.all[,'theta[6]'],type='l')
 
 
 
@@ -106,7 +125,7 @@ post.pred <- function(n, samples, seed=1234){
     p <- pmvnorm( upper=thres, mean=mu, sigma=sigma, keepAttr = F)
     u <- runif(1)
     if (u<p){
-      Y.pred[i,] <- rtmvnorm(1, mean=mu, sigma=sigma,upper=thres)
+      Y.pred[i,] <- rtmvnorm(1, mean=mu, sigma=sigma, upper=thres)
     }else{
       Y.tail <- sim.RevExpU.MGPD(n=1,d=d, a=a, beta=c(0,0), sig=sig, gamma=gamma, MGPD = T,std=T)$X
       Y.pred[i,] <- thres + Y.tail
@@ -115,34 +134,104 @@ post.pred <- function(n, samples, seed=1234){
   return(Y.pred)
 }
 
-Y.pred <- post.pred(3000, samples.all,seed=1)
+Y.pred <- post.pred(1600, samples.all,seed=2)
 plot(Y.pred)
+plot(Y.pred[(Y.pred[,1]<600)&(Y.pred[,1]>0),])
+
+plot(pm2.5_no2_mean)
 
 
-seed <- 3236
-d <- 2
-a <- colMeans(samples.all[,c('theta[1]','theta[2]')])
-beta <- c(0, 0)
-sig <- colMeans(samples.all[,c('theta[4]','theta[5]')])
-gamma <- colMeans(samples.all[,c('theta[6]','theta[7]')])
-n <- 1600
-mu <- colMeans(samples.all[,c('mu[1]','mu[2]')])
-sd1 <- mean(samples.all[,c('sds[1]')])
-sd2 <- mean(samples.all[,c('sds[2]')])
-corr.chol <- matrix(colMeans(samples.all[,c('Ustar[1, 1]','Ustar[2, 1]',
-                                     'Ustar[1, 2]','Ustar[2, 2]')]),ncol=2)
-sigma <- diag(c(sd1,sd2))%*%t(corr.chol)%*%corr.chol%*%diag(c(sd1,sd2))
-thres <- colMeans(samples.all[,c('thres[1]','thres[2]')])
 
-p <- pmvnorm(upper=thres, mean=mu, sigma=sigma, keepAttr = F)
 
-set.seed(seed)
-Y.tail<-sim.RevExpU.MGPD(n=n-floor(n*p),d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
+chi.thy <- function(a){
+  a1 <- max(1/a)
+  a2 <- min(1/a)
+  chi <- 1 - ((1+1/a1)/(1+1/a2))^(1+a2)*a1/a2/(1+a1+a2)
+  return(chi)
+}
 
-# GP scale tail data combined with the bulk data
-set.seed(seed)
-Y.bulk <- rtmvnorm(floor(n*p), mean=mu, sigma=sigma, upper=thres)
 
-# The name of the dataset should be Y for further WAIC calculation.
-Y <- rbind(Y.bulk, sweep(Y.tail$X,2,thres,"+"))
-plot(Y)
+post.dsp <- function(n.sp, samples, n.pred=2000, seed=1234){
+  set.seed(seed)
+  idx <- sample(nrow(samples),size=n.sp, replace=TRUE)
+  d <- 2
+  chi.seq <- rep(NA, length(idx))
+  tau.seq <- rep(NA, length(idx))
+  sCor.seq <- rep(NA<length(idx))
+  for (i in 1:length(idx)){
+    a <-  samples[idx[i], c('theta[1]','theta[2]')]
+    sig <- samples[idx[i], c('theta[4]','theta[5]')]
+    gamma <- samples[idx[i], c('theta[6]','theta[7]')]
+    mu <- samples[idx[i], c('mu[1]','mu[2]')]
+    sd1 <- samples[idx[i], 'sds[1]']
+    sd2 <- samples[idx[i], 'sds[2]']
+    corr.chol <- matrix(samples[idx[i],c('Ustar[1, 1]','Ustar[2, 1]',
+                                         'Ustar[1, 2]','Ustar[2, 2]')],ncol=2)
+    sigma <- diag(c(sd1,sd2))%*%t(corr.chol)%*%corr.chol%*%diag(c(sd1,sd2))
+    thres <- samples[idx[i], c('thres[1]','thres[2]')]
+    p <- pmvnorm( upper=thres, mean=mu, sigma=sigma, keepAttr = F)
+    u <- runif(1)
+    
+    Y.tail<-sim.RevExpU.MGPD(n=n.pred-floor(n.pred*p),d=d, a=a, beta=c(0,0), sig=sig, gamma=gamma, MGPD = T,std=T)
+    
+    # GP scale tail data combined with the bulk data
+    Y.bulk <- rtmvnorm(floor(n.pred*p), mean=mu, sigma=sigma, upper=thres)
+    
+    # The name of the dataset should be Y for further WAIC calculation.
+    Y <- rbind(Y.bulk, sweep(Y.tail$X,2,thres,"+"))
+    chi.seq[i] <- chi.thy(a)
+    tau.seq[i] <- cor(Y[,1], Y[,2], method='kendall')
+    sCor.seq[i] <- cor(Y[,1], Y[,2], method = "spearman")
+  }
+  dps.mat <- cbind(tau.seq, sCor.seq, chi.seq)
+  return(dps.mat)
+}
+
+
+
+t1 <- Sys.time()
+dept.est <- post.dsp(3000, samples.all, n.pred=1639)
+t2 <- Sys.time()
+
+
+tau.emp <- cor(pm2.5_no2_mean$PM2.5,pm2.5_no2_mean$NO2, method='kendall')
+sCor.emp <- cor(pm2.5_no2_mean$PM2.5,pm2.5_no2_mean$NO2, method='spearman')
+chi.emp <- chiEmp(data=pm2.5_no2_mean,nq=100,qmin=0.50,qmax=0.99)
+
+plot(density(dept.est[,1]), main='predicted Kendall tau')
+abline(v=tau.emp, col='red')
+legend('topright', legend=c("Predicted", "Empirical"),
+       col=c("black", "red"), lty=1:1, cex=2)
+
+
+plot(density(dept.est[,2]),main='predicted Spearman correlation')
+abline(v=sCor.emp,col='red')
+legend('topright', legend=c("Predicted", "Empirical"),
+       col=c("black", "red"), lty=1:1, cex=2)
+
+plot(density(dept.est[,3]), main='predicted chi')
+plot(chi.emp,col='red', main='predicted chi')
+abline(h=mean(dept.est[3]),col='black')
+legend('topright', legend=c("Posterior mean", "Empirical"),
+       col=c("black", "red"), lty=1:1, cex=2)
+
+chiEmp(Y.pred[(Y.pred[,1]<600)&(Y.pred[,1]>0)&(Y.pred[,2]<100),], nq=100,qmin=0.50,qmax=0.99)
+
+chiEmp(pm2.5_no2_mean, nq=100,qmin=0.50,qmax=0.99)
+
+
+plot(density(Y.pred[,1]), main='margin PM2.5')
+lines(density(pm2.5_no2_mean$PM2.5), col='red')
+abline(v=mean(samples.all[,'thres[1]']), lty=2,col='blue')
+legend('topright', legend=c("Predicted", "Empirical","predicted threshold"),
+       col=c("black", "red",'blue'), lty=1:1:2, cex=2)
+
+
+
+plot(density(Y.pred[,2]), main='margin NO2')
+lines(density(pm2.5_no2_mean$NO2), col='red')
+abline(v=mean(samples.all[,'thres[2]']), lty=2,col='blue')
+legend('topright', legend=c("Predicted", "Empirical","predicted threshold"),
+       col=c("black", "red",'blue'), lty=1:1:2, cex=2)
+
+
--- Simulation/parallelization_test.R
+++ Simulation/parallelization_test.R
@@ -3,26 +3,6 @@ dir.out <- "/home/pgrad2/2448355h/My_PhD_Project/01_Output/Biv_Ext_Mix_Mod/Simul
 source(file.path(dir.work, "KRSW/RevExp_U_Functions.r"))
 source(file.path(dir.work, "KRSW/CommonFunctions.r"))
 
-# install_load_packages <- function(packages) {
-#   nodename <- Sys.info()['nodename']
-#   lib_path <- file.path("/home/pgrad2/2448355h/R/library", nodename)
-#   .libPaths(lib_path)
-#   # Create a separate library folder for each node, if it doesn't exist
-#   if (!dir.exists(lib_path)) {
-#     dir.create(lib_path, recursive = TRUE)
-#   }
-#   
-#   for (package in packages) {
-#     # Check if the package is installed in the specific folder
-#     if(!require(package, character.only = TRUE, lib.loc = lib_path)) {
-#       install.packages(package, lib = lib_path, dependencies = TRUE)
-#       library(package, character.only = TRUE, lib.loc = lib_path)
-#     }
-#     
-#     # Load the package
-#     library(package, character.only = TRUE, lib.loc = lib_path)
-#   }
-# }
 
 load_install_packages <- function(packages) {
   for(package in packages){
@@ -45,121 +25,48 @@ packages <- c("nimble", "mvtnorm", "tmvtnorm","foreach","doSNOW","parallel")
 load_install_packages(packages)
 
 print(detectCores())
-# t3 <- Sys.time()
-# for (i in 1:1) {
-#   print(i)
-#   seed <- i
-#   d <- 2
-#   a <- c(1.5, 2)
-#   beta <- c(0.5, 0)
-#   sig <- c(0.5, 0.4)
-#   gamma <- c(0.2, 0.1)
-#   n <- 2000
-#   mu <- c(3.5, 4.5)
-#   sd1 <- 1.22
-#   sd2 <- 1.10
-#   rho <- 0.72
-#   sigma <- matrix(c(sd1^2, rho*sd1*sd2, rho*sd1*sd2, sd2^2),ncol=2)
-# 
-#   u.x <- c(5.6, 7)
-#   p <- pmvnorm(lower=rep(-Inf,2), upper=u.x, mean=mu, sigma=sigma, keepAttr = F)
-# 
-#   set.seed(seed)
-#   Y.tail<-sim.RevExpU.MGPD(n=n-floor(n*p),d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
-# 
-#   # GP scale tail data combined with the bulk data
-#   set.seed(seed)
-#   Y.bulk <- rtmvnorm(floor(n*p), mean=mu, sigma=sigma, lower=c(-Inf,-Inf),upper=u.x)
-# 
-#   # The name of the dataset should be Y for further WAIC calculation.
-#   Y <- rbind(Y.bulk, sweep(Y.tail$X,2,u.x,"+"))
-#   # plot(Y)
-# 
-#   source(file.path(dir.work, 'Simulation/BEMM_Functions.R'))
-# 
-#   detectCores()
-#   this_cluster <- makeCluster(4)
-# 
-#   t1 <- Sys.time()
-#   chain_output <- parLapply(cl = this_cluster, X = 1:4,
-#                             fun = run_MCMC_parallel,
-#                              dat = Y, niter = 200, nburnin=0, thin=10)
-#   t2 <- Sys.time()
-#   print(t2-t1)
-# 
-# 
-#   stopCluster(this_cluster)
-# 
-#   para.name <- colnames(chain_output[[1]]$samples)
-#   rhat.seq <- c()
-#   ess.seq <- c()
-#   for (name in para.name){
-#     post.sp <- cbind(chain_output[[1]]$samples[,name],
-#                      chain_output[[2]]$samples[,name],
-#                      chain_output[[3]]$samples[,name])
-#     rhat.seq <- c(rhat.seq, rhat(post.sp))
-#     ess.seq <- c(ess.seq, ess_basic(post.sp))
-#   }
-#   convg.stat <- data.frame(para.name,rhat.seq,ess.seq )
-# 
-# 
-# 
-#   samples.all <- rbind(chain_output[[1]]$samples,
-#                        chain_output[[2]]$samples,
-#                        chain_output[[3]]$samples)
-#   # waic <- calculateWAIC(samples.all, BivExtMixmodel)
-#   filename <- paste('Scenario1.1_seed',i,'.RData',sep='')
-#   # save(Y, chain_output, convg.stat, file=file.path(dir.out, filename))
-# }
-# t4 <- Sys.time()
-# print(t4-t3)
-#36mins for 200 iter on euclid 01, 2.15 mins for 200 iters on RStudio server
-#22mins for 100 iter on euclid 01, 1.45 mins for 100 iters on RStudio server
 
-
-NumberOfCluster <- 30
+NumberOfCluster <- 40
 cl <- makeCluster(NumberOfCluster)
 registerDoSNOW(cl)
 
+# functions used in the parallelization is in BEMM_Functions.R
 source(file.path(dir.work, 'Simulation/BEMM_Functions.R'))
 
+#Use foreach to carry out a nested loop parallelization
 t1 <- Sys.time()
 chain_res <-
-  foreach(i = c(1,20)) %:%
+  foreach(i = 1:10) %:%
   foreach(j = 1:3, .packages = c('nimble','mvtnorm','tmvtnorm')) %dopar%{
+
+    # Generate some data
     seed <- i
     d <- 2
-    a <- c(0.5, 1.2)
+    a <- c(1, 10)
     beta <- c(0, 0)
     sig <- c(0.5, 1.2)
     gamma <- c(0.3, 0.1)
     n <- 2000
     mu <- c(3.5, 4.0)
     sd1 <- 1
-    sd2 <- 1.5
+    sd2 <- 3
     rho <- 0.7
     sigma <- matrix(c(sd1^2, rho*sd1*sd2, rho*sd1*sd2, sd2^2),ncol=2)
-    
-    u.x <- c(5.5, 6.7)
-    # u.x <- c(4.7,6)
+    u.x <- c(3.5, 6.7)
     p <- pmvnorm(upper=u.x, mean=mu, sigma=sigma, keepAttr = F)
-    
     set.seed(seed)
     Y.tail<-sim.RevExpU.MGPD(n=n-floor(n*p),d=d, a=a, beta=beta, sig=sig, gamma=gamma, MGPD = T,std=T)
-    
-    # GP scale tail data combined with the bulk data
     set.seed(seed)
     Y.bulk <- rtmvnorm(floor(n*p), mean=mu, sigma=sigma, upper=u.x)
-    
-    # The name of the dataset should be Y for further WAIC calculation.
     Y <- rbind(Y.bulk, sweep(Y.tail$X,2,u.x,"+"))
-    run_MCMC_parallel(seed=j, dat=Y, niter=300, nburnin = 0, thin=10)
+
+    # function run_MCMC_parallel is called in each parallelled loop
+    run_MCMC_parallel(seed=j, dat=Y, niter=200, nburnin = 0, thin=10)
   }
-# save(chain_res,file=file.path(dir.out, filename='Scenario_1.1_seed_1_9_p84.RData'))
-# save(chain_res,  file=file.path(dir.out, filename='Scenario_1.1_itr1_25_lamfix.RData'))
+stopCluster(cl)
+
 t2 <- Sys.time()
 print(t2-t1)
-
 
 # 4 cores, 1:1,1:3, 100 iterations, 1.45 min  on RStudio server
 # 4 cores, 1:2,1:2, 100 iterations, 1.48 min  on RStudio server
@@ -185,7 +92,7 @@ print(t2-t1)
 # 15 cores, 1;5,1:3, 200 iterations, 13.9 min on Euclid 29 
 # 15 cores, 1;5,1:3, 200 iterations, 5.6 min on Euclid 28 
 # 15 cores, 1;5,1:3, 200 iterations, 5.3 min on Euclid 27
-
+# 15 cores, 1;5,1:3, 200 iterations, 12.3 min on Euclid 30
 
 #15 cores, 1:5, 1:3, 200 iterations. 13 mins on Euclid 01
 #12 cores, 1:4, 1:3, 200 iterations. 11 mins on Euclid 01
