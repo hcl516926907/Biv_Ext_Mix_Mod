45632e30bec7b02d53c7395ab7e8ab379cccf919
chenglei_hu <chenglei_hu@163.com>
2023-11-23 22:38:49 +0000

    Change the color of the histogram. Recover the code for chi, chi bar, and Kendall tau plotted in the posterior predictive check way.

--- UK_Temp/Temp_Result_Analysis.R
+++ UK_Temp/Temp_Result_Analysis.R
@@ -17,6 +17,7 @@ dir.data <- "/home/pgrad2/2448355h/My_PhD_Project/00_Dataset/UK_Temp"
 emp.mypalette <- brewer.pal(9,"Greys")
 pred.mypalette <- brewer.pal(9,"Blues")
 qq.palette <- brewer.pal(8,"Accent")
+res <- 500
 
 post.pred <- function(n, samples, seed=1234){
   set.seed(seed)
@@ -84,7 +85,7 @@ post.rep <-  function(n.rep, rep.size, samples, seed=1234){
 # load(file=file.path(dir.out, filename='western-isles_argyll_0.8_0.99.RData'))
 # load(file=file.path(dir.data, "western-isles_argyll.RData"))
 
-load(file=file.path(dir.out, filename='east-sussex_oxfordshire_0.8_0.99_v1.RData'))
+load(file=file.path(dir.out, filename='east-sussex_oxfordshire_0.8_0.99_v2.RData'))
 load(file=file.path(dir.data, "east-sussex_oxfordshire.RData"))
 Y.fit <- -Y.all
 
@@ -115,46 +116,29 @@ samples.all <- rbind(chain_out[[1]]$samples[,],
                      chain_out[[2]]$samples[,],
                      chain_out[[3]]$samples[,])
 
-
-
-apply(Y.all,2,quantile,0.83)
-
-plot(samples.all[,'sds[2]'],type='l')
-plot(samples.all[,'thres[1]'],type='l')
-abline(h=quantile(Y.fit[,1], 0.8), lty=2,col='red')
-plot(samples.all[,'thres[2]'],type='l')
-abline(h=quantile(Y.fit[,2], 0.8), lty=2,col='red')
-plot(samples.all[,'theta[7]'],type='l')
-
-hist(samples.all[,'mu[2]'],breaks=100)
-hist(samples.all[,'thres[2]'],breaks=100)
-plot(density(samples.all[,'thres[2]']))
-
+################################  histogram of the posteriors ################
 df.samples.all <- data.frame(samples.all)
 colnames(df.samples.all) <- c("Ustar_1_1","Ustar_2_1","Ustar_1_2","Ustar_2_2","mu_1",'mu_2','s_1','s_2','a_1','a_2','lam','sigma_1',
                               'sigma_2','gamma_1','gamma_2','u_1','u_2')
-hist.mypalette <- brewer.pal(9,"Greys")
 
-
-res <- 500
 
 for (col in colnames(df.samples.all)){
   filename <- paste("Hist_",col,".png",sep='')
   p <- ggplot(  df.samples.all, aes(x=.data[[col]])) +
-    geom_histogram( bins=50, fill=hist.mypalette[5], color=hist.mypalette[7], alpha=0.9) +
+    geom_histogram( bins=50, fill=pred.mypalette[5], color=pred.mypalette[7], alpha=0.9) +
     ggtitle(col) +
     theme(
       plot.title = element_text(size=15,hjust = 0.5),
       axis.title.x=element_blank(),
       axis.title.y=element_blank()
     )
-  png(filename = file.path("/home/pgrad2/2448355h/My_PhD_Project/01_Output/Biv_Ext_Mix_Mod/Plots",filename), width = 6*res, height = 5*res, res=res)
+  png(filename = file.path(dir.out,filename), width = 6*res, height = 5*res, res=res)
 
   print(p)
   dev.off()
 }
 
-
+################################# diagnostic based on 1 replicate data #############
 #seed durham london 123
 #seed moray 1234
 #seed western_isles 1
@@ -261,7 +245,8 @@ ggplot(combined_df.2, aes(x = theoretical, y = sample, color = dataset)) +
 # dev.off()
 
 ##########################posterior predictive check#########################
-Y.fit.rep <- post.rep(10000, nrow(Y.fit), samples.all, seed=1234)
+n.rep <- 3000
+Y.fit.rep <- post.rep(n.rep, nrow(Y.fit), samples.all, seed=1234)
 
 
 # probs <- c(seq(0.01, 0.99, length.out = 100),0.999)
@@ -273,7 +258,7 @@ emp_quant1 <- quantile(Y.fit[,1], probs = probs)
 thy_quant1 <- qnorm(probs, mean = mean(Y.fit[,1]), sd = sd(Y.fit[,1]))
 
 pred_quant1.mat <- c() 
-for (i in 1:n.sim){
+for (i in 1:n.rep){
   pred_quant1 <-  quantile( Y.fit.rep[[i]][,1], probs = probs)
   pred_quant1.mat <- cbind(pred_quant1, pred_quant1.mat)
 }
@@ -292,7 +277,7 @@ df.qq1 <- data.frame('Predicted_Quantile'= mean1,
                      'upper_bound'=upper1)
 
 
-ggplot(df.qq1, aes(x=emp_quant1, y=Predicted_Quantile,colour='BEMM')) +
+p <- ggplot(df.qq1, aes(x=emp_quant1, y=Predicted_Quantile,colour='BEMM')) +
   geom_abline(intercept = 0, slope = 1, color = 'black') +
   geom_ribbon(aes(ymin=lower_bound, ymax=upper_bound), colour=NA,  fill=pred.mypalette[5], alpha=0.25) +
   geom_point(aes(y=Gaussian_Quantile,colour='Gaussian')) +
@@ -312,14 +297,16 @@ ggplot(df.qq1, aes(x=emp_quant1, y=Predicted_Quantile,colour='BEMM')) +
         legend.text = element_text(size=15),
         legend.key.size = unit(0.05, 'npc'),
         legend.key.width = unit(0.1, 'npc')) 
-
+png(filename = file.path(dir.out, "QQ_plot_X1_with_CB.png"), width = 6*res, height = 5*res, res=res)
+print(p)
+dev.off()
 
 
 emp_quant2 <- quantile(Y.fit[,2], probs = probs)
 thy_quant2 <- qnorm(probs, mean = mean(Y.fit[,2]), sd = sd(Y.fit[,2]))
 
 pred_quant2.mat <- c() 
-for (i in 1:n.sim){
+for (i in 1:n.rep){
   pred_quant2 <-  quantile( Y.fit.rep[[i]][,2], probs = probs)
   pred_quant2.mat <- cbind(pred_quant2, pred_quant2.mat)
 }
@@ -333,7 +320,7 @@ df.qq2 <- data.frame('Predicted_Quantile'= mean2,
                      'lower_bound'=lower2,
                      'upper_bound'=upper2)
 
-ggplot(df.qq2, aes(x=emp_quant2, y=Predicted_Quantile,colour='BEMM')) +
+p <- ggplot(df.qq2, aes(x=emp_quant2, y=Predicted_Quantile,colour='BEMM')) +
   geom_abline(intercept = 0, slope = 1, color = 'black') +
   geom_ribbon(aes(ymin=lower_bound, ymax=upper_bound), colour=NA,  fill=pred.mypalette[5], alpha=0.25) +
   geom_point(aes(y=Gaussian_Quantile,colour='Gaussian')) +
@@ -353,8 +340,163 @@ ggplot(df.qq2, aes(x=emp_quant2, y=Predicted_Quantile,colour='BEMM')) +
         legend.text = element_text(size=15),
         legend.key.size = unit(0.05, 'npc'),
         legend.key.width = unit(0.1, 'npc')) 
+png(filename = file.path(dir.out, "QQ_plot_X2_with_CB.png"), width = 6*res, height = 5*res, res=res)
+print(p)
+dev.off()
+
+chi.thy <- function(a){
+  a1 <- max(1/a)
+  a2 <- min(1/a)
+  chi <- 1 - ((1+1/a1)/(1+1/a2))^(1+a2)*a1/a2/(1+a1+a2)
+  return(chi)
+}
+chiEmp1<-function(data,nq=25,qmin, qmax){
+  n<-nrow(data)
+  datatr<-(apply(data,2,rank) - 0.5)/n   
+  qlim<-c(qmin,qmax)
+  u <- seq(qlim[1], qlim[2], length = nq)
+  cu<-sapply(c(1:nq), function(i) mean(apply(datatr,1,min) >= u[i]))
+  res <- cbind(u,cu/(1-u))
+  res[which(res[,2]>1),2] <- 1
+  return(res)
+}
+
+etaEmp1<-function(data,nq=25,qmin, qmax){
+  n<-nrow(data)
+  datatr<-(apply(data,2,rank) - 0.5)/n   
+  qlim<-c(qmin,qmax)
+  u <- seq(qlim[1], qlim[2], length = nq)
+  cu<-sapply(c(1:nq), function(i) mean(apply(datatr,1,min) >= u[i]))
+  res <- cbind(u,2*log(1-u)/log(cu)-1)
+  return(res)
+}
 
 
+
+chi.seq <- rep(NA, length(n.rep))
+tau.seq <- rep(NA, length(n.rep))
+chiu.emp <- list()
+etau.emp <- list()
+set.seed(1234)
+idx <- sample(nrow(samples.all),size=n.rep, replace=TRUE)
+for (i in 1:n.rep){
+  print(i)
+  Y <- Y.fit.rep[[i]]
+  a <-  samples.all[idx[i], c('theta[1]','theta[2]')]
+  chi.seq[i] <- chi.thy(a)
+  chiu.emp[[i]] <- chiEmp1(data=Y, nq=50, qmin=0.5, qmax=0.99)
+  etau.emp[[i]] <- etaEmp1(data=Y, nq=50, qmin=0.5, qmax=0.99)
+  tau.seq[i] <- cor(Y[,1], Y[,2], method='kendall')
+}
+
+calc_cb <- function(depd.list){
+  n <- length(depd.list)
+  u <- depd.list[[1]][,1]
+  values <- c()
+  for (i in 1:n){
+    values <- cbind(values,depd.list[[i]][,2])    
+  }
+  lb <- apply(values,1, quantile, 0.025)
+  ub <- apply(values,1, quantile, 0.975)
+  mean <- apply(values,1, mean)
+  return(data.frame('u'=u,'lb'=lb,'ub'=ub,'mean'=mean))
+}
+
+df.chi <- calc_cb(chiu.emp)
+df.chi$emp <- chiEmp1(data=Y.fit, nq=50, qmin=0.5, qmax=0.99)[,2]
+df.thy <- data.frame(u=0.99,lb=quantile(chi.seq,0.025),
+                            ub=quantile(chi.seq,0.975),
+                            mean=mean(chi.seq))
+
+cols <- c("empirical"=qq.palette[6],
+          'posterior mean'=pred.mypalette[6], 'cb'=pred.mypalette[4],
+          'theorical'=pred.mypalette[9])
+p <- ggplot(df.chi, aes(x=u, y=mean)) +
+  geom_ribbon(aes(ymin=lb, ymax=ub,y=mean,fill='cb'),alpha=0.9) +
+  geom_line(aes(y=mean,linetype="predicted",colour='posterior mean'),size=0.8) +
+  geom_line(aes(y=emp,linetype="empirical",colour='empirical'),size=0.8) +
+
+  geom_errorbar(data=df.thy, aes(x=u, ymin=lb, ymax=ub,color='theorical'), width=0.01,  size=1) +
+  geom_point(data=df.thy, aes(x=u, y=mean,color='theorical'), size=2)+
+  
+  labs(x='r',y=TeX(r'($\chi(r)$)'), color  = "Mean", linetype = "Mean") + 
+  scale_colour_manual("",values=cols)+
+  # guide = 'none' removes the legend
+  scale_fill_manual(name="Uncertainty",values=cols,guide="none")+
+  scale_linetype_manual("",values=c('empirical'="dashed",'predicted'='twodash'),guide='none')+
+  theme(axis.text.x=element_text(size=15),
+        axis.text.y=element_text(size=15),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15),
+        legend.position=c(0, 0),
+        legend.justification = c(0, 0),
+        legend.title = element_text(size=0),
+        legend.text = element_text(size=15),
+        legend.key.size = unit(0.05, 'npc'),
+        legend.key.width = unit(0.1, 'npc'))
+
+png(filename = file.path(dir.out, "chi_ppc.png"), width = 6*res, height = 5*res, res=res)
+print(p)
+dev.off()
+
+
+
+
+
+df.eta <- calc_cb(etau.emp)
+df.eta$emp <- etaEmp1(data=Y.fit, nq=50, qmin=0.5, qmax=0.99)[,2]
+
+
+cols <- c("empirical"=qq.palette[6],
+          'posterior mean'=pred.mypalette[6], 'cb'=pred.mypalette[4],
+          'theorical'=pred.mypalette[9])
+p <- ggplot(df.eta, aes(x=u, y=mean)) +
+  geom_ribbon(aes(ymin=lb, ymax=ub,y=mean,fill='cb'),alpha=0.9) +
+  geom_line(aes(y=mean,linetype="predicted",colour='posterior mean'),size=0.8) +
+  geom_line(aes(y=emp,linetype="empirical",colour='empirical'),size=0.8) +
+
+  labs(x='r',y=TeX(r'($\bar{\chi}(r)$)')) + 
+  scale_colour_manual("",values=cols)+
+  # guide = 'none' removes the legend
+  scale_fill_manual(name="Uncertainty",values=cols,guide="none")+
+  scale_linetype_manual("",values=c('empirical'="dashed",'predicted'='twodash'),guide='none')+
+  theme(axis.text.x=element_text(size=15),
+        axis.text.y=element_text(size=15),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15),
+        legend.position = "none",
+        legend.justification = c(0, 0),
+        legend.title = element_text(size=0),
+        legend.text = element_text(size=15),
+        legend.key.size = unit(0.05, 'npc'),
+        legend.key.width = unit(0.1, 'npc'))
+
+png(filename = file.path(dir.out, "chibar_ppc.png"), width = 6*res, height = 5*res, res=res)
+print(p)
+dev.off()
+
+
+df.tau <- data.frame(tau=tau.seq)
+p <- ggplot(data=df.tau, aes(x=tau))+
+  geom_histogram(bins=50, fill=pred.mypalette[5], color=pred.mypalette[7], alpha=0.9)+
+  labs(x=TeX(r'($\tau$)'),y='')+
+  geom_vline(xintercept = cor(Y.fit[,1], Y.fit[,2], method='kendall') , linetype="dashed", 
+             color = qq.palette[6], size=0.8)+
+  theme(axis.text.x=element_text(size=15),
+        axis.text.y=element_text(size=15),
+        axis.title.x=element_text(size=15),
+        axis.title.y=element_text(size=15),
+        legend.position = "none",
+        legend.justification = c(0, 0),
+        legend.title = element_text(size=0),
+        legend.text = element_text(size=15),
+        legend.key.size = unit(0.05, 'npc'),
+        legend.key.width = unit(0.1, 'npc'),
+        plot.margin = unit(c(0.02, 0.05, 0, 0), 
+                           "npc"))
+png(filename = file.path(dir.out, "tau_ppc.png"), width = 6*res, height = 5*res, res=res)
+print(p)
+dev.off()
 ##########################################################
 chi.thy <- function(a){
   a1 <- max(1/a)
@@ -480,27 +622,27 @@ print(t4-t3)
 # save(dept.emp.300,dept.emp.3000, dept.est, file=file.path(dir.out, 'Depd_Manchester_Edinburgh.RData'))
 # save(dept.emp.300,dept.emp.3000, dept.est, file=file.path(dir.out, 'Depd_Est_east-sussex_oxfordshire_seed123.RData'))
 # save(dept.emp.300,dept.emp.3000, dept.est, file=file.path(dir.out, 'Depd_Est_east-sussex_oxfordshire_seed17.RData'))
-load(file=file.path(dir.out, 'Depd_Est_east-sussex_oxfordshire_seed17.RData'))
+# load(file=file.path(dir.out, 'Depd_Est_east-sussex_oxfordshire_seed17.RData'))
 
-plot(density(dept.emp.3000[[1]][,1]), lwd=2, lty=2, main='kendall tau density',col=2)
-lines(density(dept.est[[1]][,1]), lwd=2)
-abline(v= cor(Y.fit[,1], Y.fit[,2], method='kendall'), col=2, lwd=2, lty=2)
-legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
-
-plot(density(dept.emp.3000[[1]][,2]), lwd=2, lty=2, main='spearman density',col=2)
-lines(density(dept.est[[1]][,2]), lwd=2)
-abline(v= cor(Y.fit[,1], Y.fit[,2], method='spearman'), col=2, lwd=2, lty=2)
-legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
-
-plot(density(dept.emp.3000[[1]][,3]), lwd=2, lty=2, main='chi density',col=2,ylim=c(0,14))
-lines(density(dept.est[[1]][,4]), lwd=2)
-abline(v=chiEmp(data=Y.fit,nq=50,qmin=0.50,qmax=0.99)[50,2], col=2, lwd=2, lty=2)
-legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
-
-plot(density(dept.emp.3000[[1]][,3]), lwd=2, lty=2, main='chi density',col=2,ylim=c(0,14))
-lines(density(dept.est[[1]][,3]), lwd=2)
-abline(v=chiEmp(data=Y.fit,nq=50,qmin=0.50,qmax=0.99)[50,2], col=2, lwd=2, lty=2)
-legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
+# plot(density(dept.emp.3000[[1]][,1]), lwd=2, lty=2, main='kendall tau density',col=2)
+# lines(density(dept.est[[1]][,1]), lwd=2)
+# abline(v= cor(Y.fit[,1], Y.fit[,2], method='kendall'), col=2, lwd=2, lty=2)
+# legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
+# 
+# plot(density(dept.emp.3000[[1]][,2]), lwd=2, lty=2, main='spearman density',col=2)
+# lines(density(dept.est[[1]][,2]), lwd=2)
+# abline(v= cor(Y.fit[,1], Y.fit[,2], method='spearman'), col=2, lwd=2, lty=2)
+# legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
+# 
+# plot(density(dept.emp.3000[[1]][,3]), lwd=2, lty=2, main='chi density',col=2,ylim=c(0,14))
+# lines(density(dept.est[[1]][,4]), lwd=2)
+# abline(v=chiEmp(data=Y.fit,nq=50,qmin=0.50,qmax=0.99)[50,2], col=2, lwd=2, lty=2)
+# legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
+# 
+# plot(density(dept.emp.3000[[1]][,3]), lwd=2, lty=2, main='chi density',col=2,ylim=c(0,14))
+# lines(density(dept.est[[1]][,3]), lwd=2)
+# abline(v=chiEmp(data=Y.fit,nq=50,qmin=0.50,qmax=0.99)[50,2], col=2, lwd=2, lty=2)
+# legend("topleft", c('empirical','posterior'),col=c(2,1),cex=1, lty = c(2,1))
 
 
 length(dept.emp.3000[[2]])
