1316b5bd587a845041009240a3d154b62899d1ce
chenglei_hu <chenglei_hu@163.com>
2023-12-31 16:50:04 +0000

    Correct the GAM formula in Data Challenge 1

--- Data_Challenge_1.R
+++ Data_Challenge_1.R
@@ -1,26 +1,13 @@
 library(qgam)
 
 dir.data <- "/home/pgrad2/2448355h/My_PhD_Project/00_Dataset"
+dir.out <- '/home/pgrad2/2448355h/My_PhD_Project/01_Output/Data_Challenge/Q1'
 
 
 df <- read.csv(file.path(dir.data, 'Amaurot.csv'))
 df$u<-df$WindSpeed*cos(df$WindDirection)
 df$v<-df$WindSpeed*sin(df$WindDirection)
 df.dropna <- na.omit(df)
-# 
-# fit <- qgam(Y~s(V1, k=20, bs="cr") + 
-#               s(V2, k=20, bs='cr') +
-#               s(V3, k=20, bs='cr') + 
-#               Season +
-#               s(Atmosphere, k=20, bs='cr') + 
-#               s(u, k = 20, bs = "cr") + 
-#               s(v, k = 20, bs = "cr"),
-#             data = df.dropna, 
-#             qu = 0.999)
-# 
-# pred <- predict(fit, newdata=df.dropna)
-# mean(df.dropna$Y<pred)
-# 
 
 Quan.Est <- function(Y,p){
   Y.sorted <- sort(Y,decreasing =TRUE)
@@ -37,7 +24,7 @@ Quan.Est <- function(Y,p){
   }
   return(x.q)
 }
-# Quan.Est(df.dropna$Y, 0.84)
+
 
 Interpolation <- function(Y.large,Y.small,n,p){
   n.prime <- n+1
@@ -61,6 +48,7 @@ Quan.reg.int <- function(n,p, df.train, df.pred, dir.out, seed){
   fit1 <- qgam(Y~s(V1, k=20, bs="cr") + 
                  s(V2, k=20, bs='cr') +
                  s(V3, k=20, bs='cr') + 
+                 s(V4, k=20, bs='cr') + 
                  Season +
                  s(Atmosphere, k=20, bs='cr') + 
                  s(u, k = 20, bs = "cr") + 
@@ -68,6 +56,7 @@ Quan.reg.int <- function(n,p, df.train, df.pred, dir.out, seed){
   fit2 <- qgam(Y~Y~s(V1, k=20, bs="cr") + 
                  s(V2, k=20, bs='cr') +
                  s(V3, k=20, bs='cr') + 
+                 s(V4, k=20, bs='cr') + 
                  Season +
                  s(Atmosphere, k=20, bs='cr') + 
                  s(u, k = 20, bs = "cr") + 
@@ -100,6 +89,7 @@ Quan.reg.ext <- function(n, p, df.train, df.pred, dir.out,seed){
   fit1 <- qgam(Y~s(V1, k=20, bs="cr") + 
                  s(V2, k=20, bs='cr') +
                  s(V3, k=20, bs='cr') + 
+                 s(V4, k=20, bs='cr') + 
                  Season +
                  s(Atmosphere, k=20, bs='cr') + 
                  s(u, k = 20, bs = "cr") + 
@@ -109,6 +99,7 @@ Quan.reg.ext <- function(n, p, df.train, df.pred, dir.out,seed){
   fit2 <- qgam(Y~s(V1, k=20, bs="cr") + 
                  s(V2, k=20, bs='cr') +
                  s(V3, k=20, bs='cr') + 
+                 s(V4, k=20, bs='cr') + 
                  Season +
                  s(Atmosphere, k=20, bs='cr') + 
                  s(u, k = 20, bs = "cr") + 
@@ -146,44 +137,6 @@ Quan.reg.est <- function(n, p, df.train, df.pred, dir.out, seed){
 
 
 
-
-################################################
-
-# 
-# q.seq <- c(0.75,0.9,0.95,0.99,0.999)
-# n.seq <- c(10,30,50,100)
-# res.mat <- matrix(NA, nrow=length(n.seq), ncol=length(q.seq))
-# for (i in 1:length(n.seq)){
-#   for (j in 1:length(q.seq)){
-#     pred <- Quan.reg.est(n.seq[i], q.seq[j])
-#     res.mat[i, j] <- mean(df.dropna$Y<pred)
-#     print(c('Done',n.seq[i],q.seq[j]))
-#   }
-# }
-# 
-# res.bm <- rep(NA, length(q.seq))
-# for (i in 1:length(q.seq)){
-#   fit <-  qgam(Y~s(V1, k=20, bs="cr") + 
-#                  s(V2, k=20, bs='cr') +
-#                  s(V3, k=20, bs='cr') + 
-#                  Season +
-#                  s(Atmosphere, k=20, bs='cr'),
-#                data = df.dropna, 
-#                qu = q.seq[i])
-#   pred <- predict(fit, newdata=df.dropna)
-#   res.bm[i] <- mean(df.dropna$Y<pred)
-# }
-# 
-# plot(NULL, xlab='Theoretical probability',
-#            ylab='Model-based probablity')
-# for (i in 1:length(n.seq)){
-#   lines(q.seq, res.mat[i,], color=i)
-# }
-# 
-# save(res.mat, res.bm, file = file.path(dir.data, 'Output','Challenge1','res_int_ext_quantile.RData'))
-###########################################################
-
-
 #bootstrap average
 data <- df
 B = 5 # number of bootstrap samples
@@ -191,34 +144,34 @@ B = 5 # number of bootstrap samples
 bsize = 150 # number of elements in each block
 
 
-nblock = ceiling(nrow(data)/bsize)
-blocks = split(1:nrow(data), ceiling(seq_along(1:nrow(data))/bsize))
-n.seq <- c(10,30,90)
-weight <- rep(0, length(n.seq))
-p <- 0.9999
-score.mat <- matrix(NA, nrow=B, ncol=length(n.seq))
-t1 <- Sys.time()
-pred.list <- list()
-for(b in 1:B){
-  set.seed(b)
-  idtest = sample(1:nblock, nblock, replace = T)
-  data_test <- data[as.numeric(unlist(blocks[idtest])), ]
-  pred1 <- Quan.reg.est(10, p, data, data_test)
-  pred2 <- Quan.reg.est(30, p, data, data_test)
-  pred3 <- Quan.reg.est(90, p, data, data_test)
-  pred.list[[b]] <- cbind(pred1, pred2, pred3)
-  
-  score1 <- abs(mean(data_test$Y<pred1,na.rm = TRUE)-p)
-  score2 <- abs(mean(data_test$Y<pred2,na.rm = TRUE)-p)
-  score3 <- abs(mean(data_test$Y<pred3,na.rm = TRUE)-p)
-  score.all <- c(score1, score2, score3)
-  cond <- which(score.all==min(score.all))
-  weight[cond] <- weight[cond] + 1
-  score.mat[b,] <- score.all
-  print(c('Done ',b,' bootstrap'))
-}
-t2 <- Sys.time()
-print(t2-t1)
+# nblock = ceiling(nrow(data)/bsize)
+# blocks = split(1:nrow(data), ceiling(seq_along(1:nrow(data))/bsize))
+# n.seq <- c(10,30,90)
+# weight <- rep(0, length(n.seq))
+# p <- 0.9999
+# score.mat <- matrix(NA, nrow=B, ncol=length(n.seq))
+# t1 <- Sys.time()
+# pred.list <- list()
+# for(b in 1:B){
+#   set.seed(b)
+#   idtest = sample(1:nblock, nblock, replace = T)
+#   data_test <- data[as.numeric(unlist(blocks[idtest])), ]
+#   pred1 <- Quan.reg.est(10, p, data, data_test)
+#   pred2 <- Quan.reg.est(30, p, data, data_test)
+#   pred3 <- Quan.reg.est(90, p, data, data_test)
+#   pred.list[[b]] <- cbind(pred1, pred2, pred3)
+# 
+#   score1 <- abs(mean(data_test$Y<pred1,na.rm = TRUE)-p)
+#   score2 <- abs(mean(data_test$Y<pred2,na.rm = TRUE)-p)
+#   score3 <- abs(mean(data_test$Y<pred3,na.rm = TRUE)-p)
+#   score.all <- c(score1, score2, score3)
+#   cond <- which(score.all==min(score.all))
+#   weight[cond] <- weight[cond] + 1
+#   score.mat[b,] <- score.all
+#   print(c('Done ',b,' bootstrap'))
+# }
+# t2 <- Sys.time()
+# print(t2-t1)
 
 
 
@@ -287,79 +240,97 @@ stopCluster(cl)
 t4 <- Sys.time()
 print(t4-t3)
 
-# Print the results
-print(result)
-
-save(result, file=file.path(dir.out, 'bootstrap_result.RData'))
-
-load(file=file.path(dir.out, 'bootstrap_result.RData'))
-bs.sp.1 <- c()
-bs.sp.2 <- c()
-for (i in 1:iterations){
-  bs.sp.1 <- cbind(result[[i]][,1], bs.sp.1)
-  bs.sp.2 <- cbind(result[[i]][,2], bs.sp.2)
-}
-bs.sp.3 <- 0.5*bs.sp.1 + 0.5*bs.sp.2
-
-ci.low.1 <- rowQuantiles(bs.sp.1, probs=0.25)
-ci.mid.1 <- rowQuantiles(bs.sp.1, probs=0.5)
-ci.upp.1 <- rowQuantiles(bs.sp.1, probs=0.75)
-avg.1 <- mean(ci.upp.1-ci.low.1)
-
-ci.low.2 <- rowQuantiles(bs.sp.2, probs=0.25)
-ci.mid.2 <- rowQuantiles(bs.sp.2, probs=0.5)
-ci.upp.2 <- rowQuantiles(bs.sp.2, probs=0.75)
-avg.2 <- mean(ci.upp.2 - ci.low.2)
-
-ci.low.3 <- rowQuantiles(bs.sp.3, probs=0.25)
-ci.mid.3 <- rowQuantiles(bs.sp.3, probs=0.5)
-ci.upp.3 <- rowQuantiles(bs.sp.3, probs=0.75)
-avg.3 <- mean(ci.upp.3 - ci.low.3)
-
-par(mfrow=c(3,3))
-for (i in 1:9){
-  plotCI(x=1:3, 
-         y=c(ci.mid.1[i], ci.mid.2[i], ci.mid.3[i]), 
-         ui=c(ci.upp.1[i], ci.upp.2[i], ci.upp.3[i]), 
-         li=c(ci.low.1[i], ci.low.2[i], ci.low.3[i]),
-         xaxt = "n", xlab='',ylab='CI', main=paste('CI plot for point',i))
-  axis(1, at=1:3, labels=c('pred1', 'pred2', 'pred_avg'))
-}
 
 
-
-df1 <- data.frame(
-  idx = 1:100,
-  estimate = apply(bs.sp.1, 1, mean),
-  lower = apply(bs.sp.1, 1, quantile, 0.25),
-  upper = apply(bs.sp.1, 1, quantile, 0.75),
-  model = '30_sample_extrapolation'
-)
-
-df2 <- data.frame(
-  idx = 1:100,
-  estimate = apply(bs.sp.2, 1, mean),
-  lower = apply(bs.sp.2, 1, quantile, 0.25),
-  upper = apply(bs.sp.2, 1, quantile, 0.75),
-  model = '90_sample_extrapolation'
-)
-
-df3 <- data.frame(
-  idx = 1:100,
-  estimate = apply(bs.sp.3, 1, mean),
-  lower = apply(bs.sp.3, 1, quantile, 0.25),
-  upper = apply(bs.sp.3, 1, quantile, 0.75),
-  model = 'weighted_avg_extrapolation'
-)
-
-df.all <- rbind(df1,df2,df3)
-
-ggplot(df.all, aes(x = idx, y = estimate, color = model)) +
-  geom_point(position = position_dodge(0.5)) +
-  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2, position = position_dodge(0.5)) +
-  ylab("Estimate") +
-  xlab("Index") +
-  theme(legend.position = "bottom") + 
-  ylim(c(50,370))
-
-write.csv(df.all,  file=file.path(dir.out, 'Q1_extrapolation_estimation.csv'))
+# save(result, file=file.path(dir.out, 'bootstrap_result.RData'))
+#v1 include the missing covariates V4 in the qgam model
+save(result, file=file.path(dir.out, 'bootstrap_result_v1.RData'))
+# 
+# 
+# 
+# 
+# load(file=file.path(dir.out, 'bootstrap_result.RData'))
+# bs.sp.1 <- c()
+# bs.sp.2 <- c()
+# for (i in 1:iterations){
+#   bs.sp.1 <- cbind(result[[i]][,1], bs.sp.1)
+#   bs.sp.2 <- cbind(result[[i]][,2], bs.sp.2)
+# }
+# bs.sp.3 <- 0.5*bs.sp.1 + 0.5*bs.sp.2
+# 
+# ci.low.1 <- rowQuantiles(bs.sp.1, probs=0.25)
+# ci.mid.1 <- rowQuantiles(bs.sp.1, probs=0.5)
+# ci.upp.1 <- rowQuantiles(bs.sp.1, probs=0.75)
+# avg.1 <- mean(ci.upp.1-ci.low.1)
+# 
+# ci.low.2 <- rowQuantiles(bs.sp.2, probs=0.25)
+# ci.mid.2 <- rowQuantiles(bs.sp.2, probs=0.5)
+# ci.upp.2 <- rowQuantiles(bs.sp.2, probs=0.75)
+# avg.2 <- mean(ci.upp.2 - ci.low.2)
+# 
+# ci.low.3 <- rowQuantiles(bs.sp.3, probs=0.25)
+# ci.mid.3 <- rowQuantiles(bs.sp.3, probs=0.5)
+# ci.upp.3 <- rowQuantiles(bs.sp.3, probs=0.75)
+# avg.3 <- mean(ci.upp.3 - ci.low.3)
+# 
+# par(mfrow=c(3,3))
+# for (i in 1:9){
+#   plotCI(x=1:3, 
+#          y=c(ci.mid.1[i], ci.mid.2[i], ci.mid.3[i]), 
+#          ui=c(ci.upp.1[i], ci.upp.2[i], ci.upp.3[i]), 
+#          li=c(ci.low.1[i], ci.low.2[i], ci.low.3[i]),
+#          xaxt = "n", xlab='',ylab='CI', main=paste('CI plot for point',i))
+#   axis(1, at=1:3, labels=c('pred1', 'pred2', 'pred_avg'))
+# }
+# 
+# 
+# 
+# df1 <- data.frame(
+#   idx = 1:100,
+#   estimate = apply(bs.sp.1, 1, mean),
+#   lower = apply(bs.sp.1, 1, quantile, 0.25),
+#   upper = apply(bs.sp.1, 1, quantile, 0.75),
+#   model = '30 quantiles'
+# )
+# 
+# df2 <- data.frame(
+#   idx = 1:100,
+#   estimate = apply(bs.sp.2, 1, mean),
+#   lower = apply(bs.sp.2, 1, quantile, 0.25),
+#   upper = apply(bs.sp.2, 1, quantile, 0.75),
+#   model = '90 quantiles'
+# )
+# 
+# df3 <- data.frame(
+#   idx = 1:100,
+#   estimate = apply(bs.sp.3, 1, mean),
+#   lower = apply(bs.sp.3, 1, quantile, 0.25),
+#   upper = apply(bs.sp.3, 1, quantile, 0.75),
+#   model = 'weighted_avg_extrapolation'
+# )
+# 
+# # df.all <- rbind(df1,df2,df3)
+# df.all <- rbind(df1,df2)
+# p <- ggplot(df.all, aes(x = idx, y = estimate, color = model)) +
+#   geom_point(position = position_dodge(0.5)) +
+#   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2, position = position_dodge(0.5)) +
+#   ylab("Estimate") +
+#   xlab("Index") +
+#   ylim(c(50,370))+
+#   theme(axis.text.x=element_text(size=15),
+#         axis.text.y=element_text(size=15),
+#         axis.title.x=element_text(size=15),
+#         axis.title.y=element_text(size=15),
+#         plot.title = element_text(size=18,hjust = 0.5),
+#         legend.position=c(1, 1),
+#         legend.justification = c(1, 1),
+#         legend.title = element_blank(),
+#         legend.text = element_text(size=15),
+#         legend.key.size = unit(0.05, 'npc'),
+#         legend.key.width = unit(0.1, 'npc')) 
+# res <- 500
+# png(filename = file.path(dir.out, "bootstrap_ci.png"), width = 6*res, height = 5*res, res=res)
+# print(p)
+# dev.off()
+# 
+# write.csv(df.all,  file=file.path(dir.out, 'Q1_extrapolation_estimation.csv'))
