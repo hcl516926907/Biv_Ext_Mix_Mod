72b3ca9cf95c6a22a56a578cdeb3066a9c086d13
chenglei_hu <chenglei_hu@163.com>
2024-01-06 17:35:30 +0000

    Add comments for the Posterior_Dependence.R

--- Simulation/Posterior_Dependence.R
+++ Simulation/Posterior_Dependence.R
@@ -29,34 +29,8 @@ packages <- c("nimble", "mvtnorm", "tmvtnorm","foreach","doSNOW","parallel",'sco
 
 load_install_packages(packages)
 #################################Used functions#######################
-post.pred <- function(n, samples, seed=1234){
-  set.seed(seed)
-  Y.pred <- matrix(NA,nrow=n, ncol=2)
-  idx <- sample(nrow(samples),size=n, replace=TRUE)
-  d <- 2
-  for (i in 1:length(idx)){
-    a <-  samples[idx[i], c('theta[1]','theta[2]')]
-    sig <- samples[idx[i], c('theta[4]','theta[5]')]
-    gamma <- samples[idx[i], c('theta[6]','theta[7]')]
-    mu <- samples[idx[i], c('mu[1]','mu[2]')]
-    sd1 <- samples[idx[i], 'sds[1]']
-    sd2 <- samples[idx[i], 'sds[2]']
-    corr.chol <- matrix(samples[idx[i],c('Ustar[1, 1]','Ustar[2, 1]',
-                                         'Ustar[1, 2]','Ustar[2, 2]')],ncol=2)
-    sigma <- diag(c(sd1,sd2))%*%t(corr.chol)%*%corr.chol%*%diag(c(sd1,sd2))
-    thres <- samples[idx[i], c('thres[1]','thres[2]')]
-    p <- pmvnorm( upper=thres, mean=mu, sigma=sigma, keepAttr = F)
-    u <- runif(1)
-    if (u<p){
-      Y.pred[i,] <- rtmvnorm(1, mean=mu, sigma=sigma,upper=thres)
-    }else{
-      Y.tail <- sim.RevExpU.MGPD(n=1,d=d, a=a, beta=c(0,0), sig=sig, gamma=gamma, MGPD = T,std=T)$X
-      Y.pred[i,] <- thres + Y.tail
-    }
-  }
-  return(Y.pred)
-}
 
+# Calculate theoretical chi of the bivariate extreme mixture model.
 chi.thy <- function(a){
   a1 <- max(1/a)
   a2 <- min(1/a)
@@ -75,12 +49,9 @@ for (file in all.files){
 }
 
 
-# dim(chain_out[[1]]$samples)
-# itr <- 1
-# samples.all <- rbind(chain_out[[itr]][[1]]$samples[,],
-#                      chain_out[[itr]][[2]]$samples[,],
-#                      chain_out[[itr]][[3]]$samples[,])
 
+
+# Calculate the empirical chi
 chiEmp1<-function(data,nq=25,qmin, qmax){
   n<-nrow(data)
   datatr<-(apply(data,2,rank) - 0.5)/n   
@@ -92,6 +63,7 @@ chiEmp1<-function(data,nq=25,qmin, qmax){
   return(res)
 }
 
+# Calculate the empirical chi bar
 etaEmp1<-function(data,nq=25,qmin, qmax){
   n<-nrow(data)
   datatr<-(apply(data,2,rank) - 0.5)/n   
@@ -102,7 +74,8 @@ etaEmp1<-function(data,nq=25,qmin, qmax){
   return(res)
 }
 
-
+# Calculate the empirical chi, chi bar and Kendall's tau on the replicate data sampled from the 
+# posterior predictive distribution.
 post.dsp <- function(n.sp, samples, n.pred=2000, seed=1234){
   set.seed(seed)
   idx <- sample(nrow(samples),size=n.sp, replace=TRUE)
@@ -130,10 +103,10 @@ post.dsp <- function(n.sp, samples, n.pred=2000, seed=1234){
     
     Y.tail<-sim.RevExpU.MGPD(n=n.pred-floor(n.pred*p),d=d, a=a, beta=c(0,0), sig=sig, gamma=gamma, MGPD = T,std=T)
     
-    # GP scale tail data combined with the bulk data
+
     Y.bulk <- rtmvnorm(floor(n.pred*p), mean=mu, sigma=sigma, upper=thres)
     
-    # The name of the dataset should be Y for further WAIC calculation.
+    # GP scale tail data combined with the bulk data
     Y <- rbind(Y.bulk, sweep(Y.tail$X,2,thres,"+"))
     chi.seq[i] <- chi.thy(a)
     chiu.emp[[i]] <- chiEmp1(data=Y, nq=50, qmin=0.5, qmax=0.99)
@@ -158,9 +131,7 @@ for (itr in 1:length(chain_out)){
 }
 rm(chain_out1,chain_out2,chain_res)
 
-
-
-
+#################################Parallelly run the code#######################
 NumberOfCluster <- 40
 cl <- makeCluster(NumberOfCluster)
 registerDoSNOW(cl)
